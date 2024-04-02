27f6cc3e378ee22b8a8ea616a6b04d1b22c17467
Vadim Mychko <vadim_mychko@proton.me>
2024-04-02 22:26:31 +0200

    cleanup

--- include/tracker.h
+++ include/tracker.h
@@ -1,79 +1,54 @@
 #ifndef TRACKER_H
 #define TRACKER_H
 
-// implements similar functionality to cv::selectROI, but instead of a rectangle
-// produces selected points (with visualization)
-#include "detect_selector.h"
-
-// each ROS nodelet must have these
 #include <ros/ros.h>
 #include <ros/package.h>
 #include <nodelet/nodelet.h>
 
-// some OpenCV includes
-#include <opencv2/opencv.hpp>
-#include <opencv2/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/video/tracking.hpp>
-
-// ROS includes for working with OpenCV and images
-#include <image_transport/image_transport.h>
-#include <image_geometry/pinhole_camera_model.h>
 #include <cv_bridge/cv_bridge.h>
 
-// custom helper functions from our library
-#include <mrs_lib/param_loader.h>
+#include <image_transport/image_transport.h>
+#include <image_geometry/pinhole_camera_model.h>
+#include <subscriber_filter.h>
+
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
 #include <mrs_lib/transformer.h>
 #include <mrs_lib/dynamic_reconfigure_mgr.h>
+
 #include <eagle_track/TrackParamsConfig.h>
-
-// UAV detection custom messages
 #include <lidar_tracker/Tracks.h>
-
-// general boost useful things
-#include <boost/circular_buffer.hpp>
-
-using drmgr_t = mrs_lib::DynamicReconfigureMgr<eagle_track::TrackParamsConfig>;
 
 namespace eagle_track
 {
 
-struct CvImageStamped {
-  cv::Mat image;
-  ros::Time stamp;
-};
+using drmgr_t = mrs_lib::DynamicReconfigureMgr<eagle_track::TrackParamsConfig>;
+using policy_t = message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, lidar_tracker::Tracks>;
 
-// structure for holding all the information relevant to camera model, projected detection,
-// tracker, subscribers and publishers
-// callbacks are implemented in the Tracker class with corresponding camera context
-struct CameraContext {
-  // | ---------------------- flags --------------------- |
-  bool got_info = false; // whether got camera info for point projection
-  bool should_init = false; // whether should initialize tracker (i.e. when the detection comes)
+struct CameraContext
+{
+  // | -------------------------------- flags ------------------------------- |
+  bool got_camera_info = false;
 
-  // | ---------------------- struct parameters --------------------- |
-  std::string name; // name of the camera context
-  std::vector<cv::Point2f> detect_points; // points from the last detection
-  std::vector<cv::Point2f> points; // points calculated from previous optical flow
-  ros::Time stamp; // timestamp of the last detection
-  image_geometry::PinholeCameraModel model; // camera model for projection of 3d points
-
-  // buffer for storing latest number of images for initializing the tracker
-  // assuming detections come less frequently than images, therefore buffer for images
-  boost::circular_buffer<CvImageStamped> buffer;
-
-  // needed for visualization of projections onto the camera frame
-  cv::Mat prev_frame;
-
-  // mutex to ensure thread safety
-  // also servers as a tool to read "atomically" several variables at once: detect_points, stamp
-  std::mutex mutex;
-
-  // | ---------------------- subscribers --------------------- |
-  image_transport::Subscriber sub_image;
+  // | ---------------------------- subscribers ----------------------------- |
+  image_transport::SubscriberFilter sub_image;
   ros::Subscriber sub_info;
 
-  // | ---------------------- publishers --------------------- |
+  // | ----------------------------- publishers ----------------------------- |
   image_transport::Publisher pub_image;
+  image_transport::Publisher pub_projections;
+
+  // | -------------------------- synchronization --------------------------- |
+  std::unique_ptr<message_filters::Synchronizer<policy_t>> sync;
+
+  // | ------------------------- context essentials ------------------------- |
+  std::string name;                         // name of the camera context
+  std::vector<cv::Point2f> points;          // points calculated from previous optical flow
+  image_geometry::PinholeCameraModel model; // camera model for projection of 3d points
 
   CameraContext(const std::string& name);
 };
@@ -81,41 +56,36 @@ struct CameraContext {
 class Tracker : public nodelet::Nodelet {
 
 public:
-  // onInit() is called when nodelet is launched (similar to main() in regular node)
   virtual void onInit();
 
 private:
-  // | ---------------------- flags --------------------- |
+  // | -------------------------------- flags ------------------------------- |
   bool initialized_ = false;
 
-  // | ---------------------- static parameters --------------------- |
+  // | -------------------------- static parameters ------------------------- |
   bool _manual_detect_;
   double _throttle_period_;
 
-  // | ---------------------- dynamic parameters --------------------- |
+  // | -------------------------- dynamic parameters ------------------------ |
   std::unique_ptr<drmgr_t> drmgr_;
-  void callbackConfig(const eagle_track::TrackParamsConfig& drmgr, uint32_t level);
+  void callbackConfig(const eagle_track::TrackParamsConfig& config, uint32_t level);
 
-  // | ---------------------- subscribers --------------------- |
-  ros::Subscriber sub_detections_;
-
-  void callbackImageFront(const sensor_msgs::ImageConstPtr& msg);
+  // | ---------------------------- subscribers ----------------------------- |
+  message_filters::Subscriber<lidar_tracker::Tracks> sub_detection_;
   void callbackImage(const sensor_msgs::ImageConstPtr& msg, CameraContext& cc);
-  void callbackCameraInfoFront(const sensor_msgs::CameraInfoConstPtr& msg);
+  void callbackImageDetection(const sensor_msgs::ImageConstPtr& img_msg, const lidar_tracker::TracksConstPtr& det_msg, const CameraContext& cc);
   void callbackCameraInfo(const sensor_msgs::CameraInfoConstPtr& msg, CameraContext& cc);
-  void callbackDetections(const lidar_tracker::TracksConstPtr& msg);
 
-  // | ---------------------- publishers --------------------- |
-  image_transport::Publisher pub_projections_;
-
-  void publishImage(cv::InputArray image, const std_msgs::Header& header, const std::string& encoding, image_transport::Publisher& pub);
+  // | ----------------------------- publishers ----------------------------- |
+  void publishImage(cv::InputArray image, const std_msgs::Header& header, const std::string& encoding, const CameraContext& cc);
   void publishProjections(const std::vector<cv::Point2f>& projections, const CameraContext& cc);
 
-  // | -------------------- tracker essentials -------------------- |
+  // | ------------------------- tracker essentials ------------------------- |
   CameraContext front_ = CameraContext("FrontCamera");
-  cv::Ptr<cv::SparsePyrLKOpticalFlow> opt_flow_ = cv::SparsePyrLKOpticalFlow::create();
+  CameraContext down_ = CameraContext("DownCamera");
+  cv::Ptr<cv::SparsePyrLKOpticalFlow> flow_ = cv::SparsePyrLKOpticalFlow::create();
 
-  // | -------------------- point projection -------------------- |
+  // | -------------------------- point projection -------------------------- |
   std::unique_ptr<mrs_lib::Transformer> transformer_;
   void updateDetection(const sensor_msgs::PointCloud2& points, CameraContext& cc);
 };
--- src/tracker.cpp
+++ src/tracker.cpp
@@ -11,16 +11,15 @@ void Tracker::onInit() {
   ROS_INFO_ONCE("[Tracker]: Waiting for valid time...");
   ros::Time::waitForValid();
 
-  // | ------------------- static parameters ------------------ |
+  // | -------------------------- static parameters ------------------------- |
   mrs_lib::ParamLoader pl(nh, "Tracker");
   NODELET_INFO_ONCE("[Tracker]: Loading static parameters:");
   const auto uav_name = pl.loadParam2<std::string>("UAV_NAME");
   const auto image_type = pl.loadParam2<std::string>("image_type");
-  pl.loadParam("manual_detect", _manual_detect_);
   pl.loadParam("throttle_period", _throttle_period_);
   const auto image_buffer_size = pl.loadParam2<int>("image_buffer_size");
 
-  // | ------------------- dynamic parameters ------------------ |
+  // | -------------------------- dynamic parameters ------------------------ |
   drmgr_ = std::make_unique<drmgr_t>(nh, true, "Tracker", boost::bind(&Tracker::callbackConfig, this, _1, _2));
 
   if (!pl.loadedSuccessfully() || !drmgr_->loaded_successfully()) {
@@ -28,28 +27,40 @@ void Tracker::onInit() {
     ros::shutdown();
   }
 
-  // | ---------------------- camera contexts --------------------- |
-  // resize buffers of camera contexts based on the provided parameter
-  front_.buffer.resize(image_buffer_size);
-
-  // | ---------------------- subscribers --------------------- |
+  // | ---------------------------- subscribers ----------------------------- |
   image_transport::ImageTransport it(nh);
   image_transport::TransportHints hints(image_type);
 
-  front_.sub_image = it.subscribe("camera_front", 1, &Tracker::callbackImageFront, this, hints);
-  front_.sub_info = nh.subscribe("camera_front_info", 1, &Tracker::callbackCameraInfoFront, this);
-  sub_detections_ = nh.subscribe("detections", 1, &Tracker::callbackDetections, this);
+  front_.sub_image.subscribe(it, "camera_front", 1, hints);
+  front_.sub_image.registerCallback(boost::bind(&Tracker::callbackImage, this, _1, std::ref(front_)));
+  front_.sub_info = nh.subscribe<sensor_msgs::CameraInfo>("camera_front_info", 1, boost::bind(&Tracker::callbackCameraInfo, this, _1, std::ref(front_)));
 
-  // | ---------------------- publishers --------------------- |
+  down_.sub_image.subscribe(it, "camera_down", 1, hints);
+  down_.sub_image.registerCallback(boost::bind(&Tracker::callbackImage, this, _1, std::ref(down_)));
+  down_.sub_info = nh.subscribe<sensor_msgs::CameraInfo>("camera_down_info", 1, boost::bind(&Tracker::callbackCameraInfo, this, _1, std::ref(down_)));
+
+  sub_detection_.subscribe(nh, "detection", 1);
+
+  // | ----------------------------- publishers ----------------------------- |
   front_.pub_image = it.advertise("tracker_front", 1);
-  pub_projections_ = it.advertise("projections", 1);
+  front_.pub_projections = it.advertise("projections_front", 1);
 
-  // | --------------------- tf transformer --------------------- |
+  down_.pub_image = it.advertise("tracker_down", 1);
+  down_.pub_projections = it.advertise("projections_down", 1);
+
+  // | -------------------------- synchronization --------------------------- |
+  front_.sync = std::make_unique<message_filters::Synchronizer<policy_t>>(policy_t(image_buffer_size), front_.sub_image, sub_detection_);
+  front_.sync->registerCallback(boost::bind(&Tracker::callbackImageDetection, this, _1, _2, std::ref(front_)));
+
+  down_.sync = std::make_unique<message_filters::Synchronizer<policy_t>>(policy_t(image_buffer_size), down_.sub_image, sub_detection_);
+  down_.sync->registerCallback(boost::bind(&Tracker::callbackImageDetection, this, _1, _2, std::ref(down_)));
+
+  // | --------------------------- tf transformer --------------------------- |
   transformer_ = std::make_unique<mrs_lib::Transformer>("Tracker");
   transformer_->setDefaultPrefix(uav_name);
   transformer_->retryLookupNewest(true);
 
-  // | --------------------- detection through GUI --------------------- |
+  // | ------------------------ detection through GUI ----------------------- |
   if (_manual_detect_) {
     int flags = cv::WINDOW_NORMAL | cv::WINDOW_FREERATIO | cv::WINDOW_GUI_EXPANDED;
     cv::namedWindow("manual_detect", flags);
@@ -61,120 +72,17 @@ void Tracker::onInit() {
 
 void Tracker::callbackConfig(const eagle_track::TrackParamsConfig& config, uint32_t level) {
   if (level == 1) {
-    opt_flow_->setWinSize({config.winSizeWidth, config.winSizeHeight});
-    opt_flow_->setMaxLevel(config.maxLevel);
+    flow_->setWinSize({config.winSizeWidth, config.winSizeHeight});
+    flow_->setMaxLevel(config.maxLevel);
     cv::TermCriteria criteria(cv::TermCriteria::COUNT | cv::TermCriteria::EPS, config.maxCount, config.epsilon);
-    opt_flow_->setTermCriteria(criteria);
-    opt_flow_->setFlags((config.useInitialFlow ? cv::OPTFLOW_USE_INITIAL_FLOW : 0)
+    flow_->setTermCriteria(criteria);
+    flow_->setFlags((config.useInitialFlow ? cv::OPTFLOW_USE_INITIAL_FLOW : 0)
                       | (config.getMinEigenvals ? cv::OPTFLOW_LK_GET_MIN_EIGENVALS : 0));
-    opt_flow_->setMinEigThreshold(config.minEigThreshold);
+    flow_->setMinEigThreshold(config.minEigThreshold);
   }
 }
 
-void Tracker::callbackImageFront(const sensor_msgs::ImageConstPtr& msg) {
-  callbackImage(msg, front_);
-}
-
-void Tracker::callbackImage(const sensor_msgs::ImageConstPtr& msg, CameraContext& cc) {
-  if (!initialized_) {
-    return;
-  }
-
-  const std::string encoding = "bgr8";
-  cv_bridge::CvImageConstPtr bridge_image_ptr = cv_bridge::toCvShare(msg, encoding);
-  cv::Mat image = bridge_image_ptr->image;
-
-  // store the last bgr image for visualization of projections onto the camera frame
-  cc.prev_frame = image;
-
-  // push grayscale image into the buffer
-  cv::Mat grayscale;
-  cv::cvtColor(image, grayscale, cv::COLOR_BGR2GRAY);
-  cc.buffer.push_back({grayscale, msg->header.stamp});
-
-  auto from = cc.buffer.end() - 2;
-  if (_manual_detect_ && cc.points.empty()) {
-    cc.points = selectPoints("manual_detect", image);
-    from = cc.buffer.end();
-  } else if (cc.should_init && !cc.detect_points.empty()) {
-    ros::Time stamp;
-    {
-      std::lock_guard lock(cc.mutex);
-      cc.should_init = false;
-      cc.points = std::move(cc.detect_points);
-      stamp = cc.stamp;
-    }
-
-    // find the closest image in terms of timestamps
-    double target = stamp.toSec();
-    from = std::min_element(cc.buffer.begin(), cc.buffer.end(),
-      [&target](const CvImageStamped& a, const CvImageStamped& b) {
-        return std::abs(a.stamp.toSec() - target) < std::abs(b.stamp.toSec() - target);
-    });
-  }
-
-  // iterate over remaining images in the buffer if needed
-  for (auto it = from; !cc.points.empty() && it < cc.buffer.end() - 1; ++it) {
-    auto prev = it;
-    auto curr = it + 1;
-
-    // | --------- perform optical flow for the two images -------- |
-    std::vector<cv::Point2f> next_points;
-    std::vector<uchar> status;
-    opt_flow_->calc(prev->image, curr->image, cc.points, next_points, status);
-
-    // | --------- filter points by their status after the optical flow -------- |
-    std::vector<cv::Point2f> filtered_points;
-    for (std::size_t i = 0; i < next_points.size(); ++i) {
-      if (status[i] == 1) {
-        filtered_points.push_back(next_points[i]);
-      }
-    }
-
-    cc.points = std::move(filtered_points);
-  }
-
-  if (cc.points.empty()) {
-    publishImage(image, msg->header, encoding, cc.pub_image);
-  } else {
-    // we don't want to modify the original image, therefore we need to copy it
-    cv::Mat track_image = image.clone();
-    for (const auto& point : cc.points) {
-      cv::circle(track_image, point, 5, cv::Scalar(255, 0, 0), -1);
-    }
-
-    publishImage(track_image, msg->header, encoding, cc.pub_image);
-  }
-}
-
-void Tracker::callbackCameraInfoFront(const sensor_msgs::CameraInfoConstPtr& msg) {
-  callbackCameraInfo(msg, front_);
-}
-
-void Tracker::callbackCameraInfo(const sensor_msgs::CameraInfoConstPtr& msg, CameraContext& cc) {
-  if (!initialized_ || cc.got_info) {
-    return;
-  }
-
-  cc.got_info = true;
-  cc.model.fromCameraInfo(*msg);
-  NODELET_INFO_STREAM_ONCE("[" << cc.name << "]: Initialized camera info");
-}
-
-void Tracker::callbackDetections(const lidar_tracker::TracksConstPtr& msg) {
-  if (!initialized_) {
-    return;
-  }
-
-  for (auto track : msg->tracks) {
-    if (track.selected) {
-      updateDetection(track.points, front_);
-      break;
-    }
-  }
-}
-
-void Tracker::publishImage(cv::InputArray image, const std_msgs::Header& header, const std::string& encoding, image_transport::Publisher& pub) {
+void Tracker::publishImage(cv::InputArray image, const std_msgs::Header& header, const std::string& encoding, const CameraContext& cc) {
   // Prepare a cv_bridge image to be converted to the ROS message
   cv_bridge::CvImage bridge_image_out;
   bridge_image_out.image = image.getMat();
@@ -183,78 +91,7 @@ void Tracker::publishImage(cv::InputArray image, const std_msgs::Header& header,
 
   // Now convert the cv_bridge image to a ROS message and publish it
   sensor_msgs::ImageConstPtr out_msg = bridge_image_out.toImageMsg();
-  pub.publish(out_msg);
-}
-
-void Tracker::publishProjections(const std::vector<cv::Point2f>& projections, const CameraContext& cc) {
-  if (cc.prev_frame.empty()) {
-    return;
-  }
-
-  cv::Mat project_image = cc.prev_frame.clone();
-  for (const cv::Point2d& point : projections) {
-    cv::circle(project_image, point, 5, cv::Scalar(0, 0, 255), -1);
-  }
-
-  cv_bridge::CvImage bridge_image_out;
-  bridge_image_out.image = project_image;
-  bridge_image_out.encoding = "bgr8";
-  sensor_msgs::ImageConstPtr out_msg = bridge_image_out.toImageMsg();
-  pub_projections_.publish(out_msg);
-}
-
-void Tracker::updateDetection(const sensor_msgs::PointCloud2& points, CameraContext& cc) {
-  if (!cc.got_info) {
-    NODELET_WARN_STREAM_THROTTLE(_throttle_period_, "[" << cc.name << "]: Failed to transform the pointcloud to the camera frame");
-    return;
-  }
-
-  // | --------- get the transformation from to the camera frame -------- |
-  auto ret = transformer_->getTransform(points.header.frame_id, cc.model.tfFrame(), points.header.stamp);
-  if (!ret.has_value()) {
-    NODELET_WARN_STREAM_THROTTLE(_throttle_period_, "[" << cc.name << "]: Failed to transform the pointcloud to the camera frame");
-    return;
-  }
-
-  // Convert PointCloud2 to pcl::PointCloud
-  pcl::PointCloud<pcl::PointXYZ> cloud;
-  pcl::fromROSMsg(points, cloud);
-
-  // | --------- transform the pointcloud to the camera frame -------- |
-  pcl_ros::transformPointCloud(cloud, cloud, ret.value().transform);
-
-  double cam_width = cc.model.fullResolution().width;
-  double cam_height = cc.model.fullResolution().height;
-
-  std::vector<cv::Point2f> projections;
-  projections.reserve(cloud.points.size());
-  for (const pcl::PointXYZ& point : cloud.points) {
-    if (point.z < 0) {
-      continue;
-    }
-
-    // | ----------- backproject the point from 3D to 2D ---------- |
-    cv::Point2d pt2d = cc.model.project3dToPixel(cv::Point3d(point.x, point.y, point.z));
-    // | ----------- unrectify the 2D point coordinates ----------- |
-    // This is done to correct for camera distortion. IT has no effect in simulation, where the camera model
-    // is an ideal pinhole camera, but usually has a BIG effect when using real cameras, so don't forget this part!
-    cv::Point2d pt2d_unrec = cc.model.unrectifyPoint(pt2d);
-
-    // check if the projected point is inside the camera frame
-    if (pt2d_unrec.x >= 0 && pt2d_unrec.x < cam_width
-          && pt2d_unrec.y >= 0 && pt2d_unrec.y < cam_height) {
-      projections.push_back(pt2d_unrec);
-    }
-  }
-
-  {
-    std::lock_guard lock(cc.mutex);
-    cc.should_init = true;
-    cc.detect_points = projections;
-    cc.stamp = points.header.stamp;
-  }
-
-  publishProjections(projections, cc);
+  cc.pub_image.publish(out_msg);
 }
 
 } // namespace eagle_track
