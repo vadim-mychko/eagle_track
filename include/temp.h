ab3e7a5309eae93f8688ec7b14ab28cac6033d78
Vadim Mychko <vadim_mychko@proton.me>
2024-04-02 20:38:36 +0200

    make the attributes private

--- include/temp.h
+++ include/temp.h
@@ -46,7 +46,7 @@ class Tracker : public nodelet::Nodelet {
 public:
   virtual void onInit();
 
-protected:
+private:
   // | -------------------------------- flags ------------------------------- |
   bool initialized_ = false;
 
