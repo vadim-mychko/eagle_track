ca8c6a1c9fdd3731d399c453a42aa2c2eef94247
Vadim Mychko <vadim_mychko@proton.me>
2024-04-08 19:24:56 +0200

    add comments

--- include/tracker.h
+++ include/tracker.h
@@ -32,24 +32,25 @@ namespace eagle_track
 
 struct CvImageStamped
 {
-  cv::Mat image;
-  ros::Time stamp;
+  cv::Mat image;   // image from the camera (grayscale or RGB)
+  ros::Time stamp; // exact timestamp related to the image
 };
 
 struct CameraContext
 {
   // | -------------------------------- flags ------------------------------- |
-  bool got_camera_info = false;
+  bool got_camera_info = false; // whether received camera parameters already
 
   // | ---------------------------- subscribers ----------------------------- |
-  image_transport::SubscriberFilter sub_image;
-  ros::Subscriber sub_info;
+  image_transport::SubscriberFilter sub_image; // for receiving images from the camera
+  ros::Subscriber sub_info;                    // for receiving camera parameters
 
   // | ----------------------------- publishers ----------------------------- |
-  image_transport::Publisher pub_image;
-  image_transport::Publisher pub_projections;
+  image_transport::Publisher pub_image;       // for publishing images + tracking result (points, bounding box, etc.)
+  image_transport::Publisher pub_projections; // for publishing 2D projections
 
   // | -------------------------- synchronization --------------------------- |
+  bool should_init = false;                      // whether should re-initialize the tracker in the image callback
   std::vector<cv::Point2f> detection_points;     // points from the latest detection
   ros::Time detection_stamp;                     // timestamp of the latest detection
   std::mutex sync_mutex;                         // mutex for synchronization between callbacks
@@ -61,7 +62,7 @@ struct CameraContext
   std::vector<cv::Point2f> prev_points;     // points calculated from previous optical flow
   cv::Mat prev_image;                       // previous image for calculating optical flow
 
-  CameraContext(const std::string& name);
+  CameraContext(const std::string& name);   // constructor with the name of the camera context
 };
 
 class Tracker : public nodelet::Nodelet {
@@ -71,33 +72,33 @@ public:
 
 private:
   // | -------------------------------- flags ------------------------------- |
-  bool initialized_ = false;
+  bool initialized_ = false; // whether the nodelet is initialized (after calling onInit())
 
   // | -------------------------- static parameters ------------------------- |
-  bool _manual_detect_;
-  double _throttle_period_;
+  bool _manual_detect_;     // should use detections from the GUI or not
+  double _throttle_period_; // parameter regulating frequency of log messages
 
   // | -------------------------- dynamic parameters ------------------------ |
-  using drmgr_t = mrs_lib::DynamicReconfigureMgr<eagle_track::TrackParamsConfig>;
-  std::unique_ptr<drmgr_t> drmgr_;
-  void callbackConfig(const eagle_track::TrackParamsConfig& config, uint32_t level);
+  using drmgr_t = mrs_lib::DynamicReconfigureMgr<eagle_track::TrackParamsConfig>;    // short name for the dynamic config
+  std::unique_ptr<drmgr_t> drmgr_;                                                   // dynamic config manager
+  void callbackConfig(const eagle_track::TrackParamsConfig& config, uint32_t level); // dynamic config callback
 
   // | ---------------------------- subscribers ----------------------------- |
-  message_filters::Subscriber<lidar_tracker::Tracks> sub_detection_;
-  void callbackImage(const sensor_msgs::ImageConstPtr& msg, CameraContext& cc);
-  void callbackDetection(const lidar_tracker::TracksConstPtr& msg, CameraContext& cc);
-  void callbackCameraInfo(const sensor_msgs::CameraInfoConstPtr& msg, CameraContext& cc);
+  ros::Subscriber sub_detection_;                                                         // for receiving incoming detections
+  void callbackImage(const sensor_msgs::ImageConstPtr& msg, CameraContext& cc);           // callback for the images from the cameras
+  void callbackDetection(const lidar_tracker::TracksConstPtr& msg, CameraContext& cc);    // callback for the detections
+  void callbackCameraInfo(const sensor_msgs::CameraInfoConstPtr& msg, CameraContext& cc); // callback for the camera info
 
   // | ----------------------------- publishers ----------------------------- |
   void publishImage(cv::InputArray image, const std_msgs::Header& header, const std::string& encoding, image_transport::Publisher& pub);
 
   // | ------------------------- tracker essentials ------------------------- |
-  CameraContext front_ = CameraContext("FrontCamera");
-  CameraContext down_ = CameraContext("DownCamera");
-  cv::Ptr<cv::SparsePyrLKOpticalFlow> flow_ = cv::SparsePyrLKOpticalFlow::create();
+  CameraContext front_ = CameraContext("FrontCamera");                              // camera context for the front camera
+  CameraContext down_ = CameraContext("DownCamera");                                // camera context for the down camera
+  cv::Ptr<cv::SparsePyrLKOpticalFlow> flow_ = cv::SparsePyrLKOpticalFlow::create(); // parametrized Lucas-Kanade tracker
 
   // | ------------------------ coordinate transforms ----------------------- |
-  std::unique_ptr<mrs_lib::Transformer> transformer_;
+  std::unique_ptr<mrs_lib::Transformer> transformer_; // for transforming coordinates between sensors
 };
 
 } // namespace eagle_track
