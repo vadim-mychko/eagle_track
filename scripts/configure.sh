97a8e84b84be69b8e80e5d148420d55a54e52f56
Vadim Mychko <vadim_mychko@proton.me>
2024-05-13 19:12:40 +0200

    use environment variables

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-OPENCV_VER="4.9.0"
-OPENCV_DIR="$HOME/opencv-$OPENCV_VER"
+export OPENCV_DIR="$HOME/opencv-4.9.0"
 export LD_LIBRARY_PATH="$OPENCV_DIR/install/lib":$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH="$OPENCV_DIR/build/unix-install":$PKG_CONFIG_PATH
