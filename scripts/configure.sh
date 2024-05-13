88fb69577ce0cff0d20b891d02b5689d48c0d78e
Vadim Mychko <vadim_mychko@proton.me>
2024-05-13 19:27:30 +0200

    remove unnecessary configure bash script

--- scripts/configure.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export OPENCV_DIR="$HOME/opencv-4.9.0/build"
-export LD_LIBRARY_PATH="$HOME/opencv-4.9.0/install/lib":$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH="$HOME/opencv-4.9.0/build/unix-install":$PKG_CONFIG_PATH
